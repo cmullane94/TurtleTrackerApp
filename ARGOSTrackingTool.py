fad2e67ea883c5d5349ee88439cbd84305dbe0cc
cmullane94 <cmullane94@gmail.com>
2020-09-27 00:31:04 -0400

    Writing & Debugging, Task 5: Building Python dictionaries of ARGOS observations

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,6 +21,13 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Creating empty dictionary for dates
+date_dict = {}
+
+#Creating empty dictionary for location coordinates
+coord_dict = {}
+
+#Iterate through all lines in the lineList
 for lineString in line_list:
     if lineString[0] in ('#', 'u'): continue
 
@@ -36,3 +43,11 @@ for lineString in line_list:
     
     #Print the location of sara using an f-string
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    
+    #Add the observation date to an empty dictionary
+    date_dict[record_id] = obs_date
+    
+    #Add the observation coordinates to an empty dictionary
+    coord_dict[record_id] = (obs_lat, obs_lon)
+    
+    
